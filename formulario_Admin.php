554e7be9bc96d4b73639024921f1923d8ff06641
Abeluky <84735804+Abeluky@users.noreply.github.com>
2021-05-26 18:10:58 -0600

    Update formulario_Admin.php

--- formulario_Admin.php
+++ formulario_Admin.php
@@ -3,30 +3,25 @@
 <head>
 
 	<title>Form admin</title>
-
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <link rel="stylesheet" href="Estilos_CSS/Estilos.css">
-
 </head>
 <body>
-
 	<section class="form-Regis">
 		<h5>
 			Registro de Usuario
 		</h5>
-		<form action="#" method="post" >
+		<form action="#" method="post">
 		<input class="controls" type="text" name="nombres" id="nombres" value="" placeholder="Ingrese sus nombres" required="">
         <input type="text" class="controls" name="aPaterno" id="aPaterno" value="" placeholder="Ingrese su apellido paterno" required="">
         <input type="text" class="controls" name="aMaterno" id="aMaterno" value="" placeholder="Ingrese su apellido materno" required="">
         <input type="email" class="controls" name="email" id="email" value="" placeholder="Ingrese su correo electronico" required="">
         <input type="text" class="controls" name="username" id="username" value="" placeholder="Ingrese su nombre de usuario" required="">
 		<input class="controls" type="password" name="password" id ="password"  value="" placeholder="Contraseña" required="">
+		<p><a href="Login_Abel.html">Volver</a></p>
 		<input class="buttons" type="submit" name="ingresar">
 		</form>
-
 	</section>
-
 </body>
 </html>
