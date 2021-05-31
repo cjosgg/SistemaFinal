78be096725c1b8eb89b0dab31a55b39b9a5244b0
cjosgg <63071649+cjosgg@users.noreply.github.com>
2021-05-31 09:05:50 -0600

    Add files via upload

---
+++ menu_PrincipalMaestros.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+
+	<title>Menu principal-Maestro</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="Estilos_CSS/Estilos.css">
+</head>
+<body>
+		
+		<ul class="menu_principal">
+			
+			<li><a href="#">Inicio</a></li>
+			<li><a href="#">Ver clase</a></li>
+			<li><a href="#">Calificar alumnos</a></li>
+			<li><a href="#">Cerrar sesion</a></li>
+		</ul>
+	
+</body>
+</html>
