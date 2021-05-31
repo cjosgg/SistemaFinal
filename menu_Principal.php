ef5e68c374518f2bfd49c9b8608c3f9473fd52d1
Abeluky <84735804+Abeluky@users.noreply.github.com>
2021-05-30 19:46:57 -0600

    Add files via upload

---
+++ menu_Principal.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+
+	<title>Menu principal</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="Estilos_CSS/Estilos.css">
+
+</head>
+<body>
+		
+		<ul class="menu_principal">
+			
+			<li><a href="#">Inicio</a></li>
+			<li><a href="#">Alumnos</a></li>
+			<li><a href="#">Maestros</a></li>
+			<li><a href="#">Usuarios</a>
+				<ul>
+					
+					<li><a href="#">Alumno</a></li>
+					<li><a href="#">Maestro</a></li>
+					<li><a href="#">Administrador</a></li>
+					
+				</ul>
+			</li>
+			
+			<li><a href="#">Cerrar sesion</a></li>
+
+		</ul>
+
+	
+
+</body>
+</html>
